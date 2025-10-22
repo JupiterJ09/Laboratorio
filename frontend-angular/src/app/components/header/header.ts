9eaef8b9353e3ad5c202923161e78b4fff27702d
Alcazardavid <david.alcazar01@unach.mx>
2025-10-22 10:54:09 -0600

    Tarea 6.1 completa Diseñar sidebar y layout principal

--- frontend-angular/src/app/components/header/header.ts
+++ frontend-angular/src/app/components/header/header.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true, // 1. Hazlo standalone
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header {
+// 2. ESTA LÍNEA ES LA SOLUCIÓN AL SEGUNDO ERROR
+export class HeaderComponent {
 
 }
--- frontend-angular/src/app/models/alerta.interface.ts
+++ frontend-angular/src/app/models/alerta.interface.ts
@@ -1,2 +1,6 @@
-export interface Alerta { }
-//añadir todas las propiedades para que funcione el back
+export interface Alerta {
+    id: any;
+    leida: any;
+  }
+  //añadir todas las propiedades para que funcione el back
+  // TODO: Añadir el resto de propiedades (ej. mensaje, fecha, etc.)

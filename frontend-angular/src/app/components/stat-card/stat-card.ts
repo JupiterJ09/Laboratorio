c41d842345a3590a0124df81ab5d83420e4f2f6a
Alcazardavid <david.alcazar01@unach.mx>
2025-10-22 12:51:40 -0600

    tarea completada 6.2 creacion de componenete stat card

--- frontend-angular/src/app/components/stat-card/stat-card.ts
+++ frontend-angular/src/app/components/stat-card/stat-card.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+/**
+ *stat-card.ts
+ *componente (hijo) reutilizable para mostrar una tarjeta de estadistica
+ *recibe los datos (titulo, valor, etc) desde un componente padre a traves de Input
+ *hecho 22/10/25 Alcazardavid, 6.2
+ */
+
+import { Component, Input } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-stat-card',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgClass
+  ],
   templateUrl: './stat-card.html',
   styleUrl: './stat-card.css'
 })
-export class StatCard {
-
-}
+export class StatCardComponent {
+  @Input() titulo!: string;
+  @Input() valor!: string | number;
+  @Input() subtexto!: string;
+  @Input() color: string = 'blue';
+  }
